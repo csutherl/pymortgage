1814c0622f097c7c6e47718bb3035366a013abac
coty <sutherland.coty@gmail.com>
2014-01-01 15:27:01 -0500

    Fixed logic errors to make it actually work when adjustable rate info is not provided :)

--- pymortgage/server/Api_helper.py
+++ pymortgage/server/Api_helper.py
@@ -1,6 +1,7 @@
 __author__ = 'coty'
 
 from pymortgage.server.amortization import Amortization_Schedule
+import traceback
 
 
 def parse_params(params):
@@ -17,36 +18,7 @@ def parse_params(params):
     '''
     adjustable = False
 
-    try:
-        if 1 < params['r'] < 0:
-            raise Exception("Rate is not expressed as decimal.")
-        elif params['P'] is None:
-            raise Exception("Principal not provided.")
-        elif params['n'] is None:
-            raise Exception("Terms not provided.")
-        elif params['t'] is None:
-            raise Exception("Taxes not provided.")
-        elif params['i'] is None:
-            raise Exception("Insurance not provided.")
-    except Exception as e:
-        print "There was an exception: %s" % e.message
-        return None
-
-    try:
-        if (params['af'] is None and params['ac'] is None and params['lc'] is None) or \
-            (params['af'] is not None and params['ac'] is not None and params['lc'] is not None):
-            if 1 < params['ac'] < 0:
-                raise Exception("Adjustment cap is not expressed as decimal.")
-            elif 1 < params['lc'] < 0:
-                raise Exception("Lifetime cap is not expressed as decimal.")
-            else:
-                adjustable = True
-        else:
-            raise Exception("Partial values provided for Adjustable mortgage.")
-    except Exception as e:
-        print "There was an exception: %s" % e.message
-        return None
-
+    # check keys and conversions
     p = params  # shorting the var name :)
     try:
         rate = float(p['r'])
@@ -54,18 +26,39 @@ def parse_params(params):
         term = int(p['n'])
         tax = int(p['t'])
         ins = int(p['i'])
+    except KeyError as ke:
+        print "A required parameter is missing: %s" % ke.message
+        return None
+    except ValueError as ve:
+        print "A required parameter has an invalid value: %s" % ve.message
+        return None
+
+    try:
+        adj_freq = int(p['af'])
+        adj_cap = float(p['ac'])
+        life_cap = float(p['lc'])
+
+        adjustable = True
+    except KeyError:
+        adjustable = False
+    except ValueError as ve:
+        print "A parameter has an invalid value: %s" % ve.message
+        return None
+
+    # check values
+    try:
+        if 1 < rate < 0:
+            raise Exception("Rate is not expressed as decimal.")
+        if adjustable:
+            if 1 < adj_cap < 0:
+                raise Exception("Adjustment cap is not expressed as decimal.")
+            elif 1 < life_cap < 0:
+                raise Exception("Lifetime cap is not expressed as decimal.")
     except Exception as e:
-        print "There was an exception: %s" % e.message
+        print e.message
         return None
 
     if adjustable:
-        try:
-            adj_freq = int(p['af'])
-            adj_cap = float(p['ac'])
-            life_cap = float(p['lc'])
-        except Exception as e:
-            print "There was an exception: %s" % e.message
-            return None
         return Amortization_Schedule(rate, prin, term, tax, ins, adj_freq, adj_cap, life_cap)
     else:
         return Amortization_Schedule(rate, prin, term, tax, ins)
